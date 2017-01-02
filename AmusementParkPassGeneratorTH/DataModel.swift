d192fd4bd7d494412dfa26a0c230e6584eae4842
Jari Koopman <MBJ@MacBook-Pro-van-Jari.local>
2017-01-02 10:36:30 +0100

    Data model finished

--- AmusementParkPassGeneratorTH/DataModel.swift
+++ AmusementParkPassGeneratorTH/DataModel.swift
@@ -16,16 +16,13 @@ enum Area {
     case Amusement, Kitchen, RideControl, Maintenance, Office
 }
 
-enum ManagerType {
-    case Normal
-}
 
 enum EmployeeType {
     case Food, Ride, Maintenance
 }
 
 enum GuestType {
-    case Classic, Child(Age), VIP
+    case Classic, Child, VIP
 }
 
 //MARK: Protocols
@@ -38,7 +35,16 @@ protocol Guest: Entrant {
     var type: GuestType { get }
 }
 
-protocol Employee: Entrant, Discount {
+protocol Administrator: Entrant, Discount {
+    var firstName: String { get }
+    var lastName: String { get }
+    var adress: String { get }
+    var city: String { get }
+    var state: String { get }
+    var zip: String { get }
+}
+
+protocol Worker: Entrant, Discount {
     var firstName: String { get }
     var lastName: String { get }
     var adress: String { get }
@@ -52,22 +58,9 @@ protocol Discount {
     var merchDiscount: Percent { get }
 }
 
-protocol VIPDiscount: Discount {}
-protocol ManagerDiscount: Discount{}
-protocol EmployeeDiscount: Discount {}
-
 //MARK: Extensions
 
-extension VIPDiscount {
-    var foodDiscount: Percent {
-        return 10
-    }
-    var merchDiscount: Percent {
-        return 20
-    }
-}
-
-extension Employee {
+extension Worker {
     var foodDiscount: Percent {
         return 15
     }
@@ -76,12 +69,16 @@ extension Employee {
     }
 }
 
-extension ManagerDiscount {
+extension Manager {
     var foodDiscount: Percent {
         return 25
     }
     var merchDiscount: Percent {
         return 25
+    }
+    
+    var areaAccess: [Area] {
+        return [Area.Amusement, Area.Kitchen, Area.Maintenance, Area.Office, Area.RideControl]
     }
 }
 
@@ -90,6 +87,97 @@ extension Guest {
         return [Area.Amusement]
     }
 }
+
+
+//MARK: Classes
+
+class Employee: Worker {
+    var firstName: String
+    var lastName: String
+    var adress: String
+    var city: String
+    var state: String
+    var zip: String
+    var areaAccess: [Area]
+    
+    init(first: String, last: String, adress: String, city: String, state: String, zip: String, type: EmployeeType) {
+        self.firstName = first
+        self.lastName = last
+        self.adress = adress
+        self.city = city
+        self.state = state
+        self.zip = zip
+        
+        switch type {
+        case .Food:
+            self.areaAccess = [Area.Amusement, Area.Kitchen]
+        case .Maintenance:
+            self.areaAccess = [Area.Amusement, Area.Maintenance, Area.RideControl]
+        case .Ride:
+            self.areaAccess = [Area.Amusement, Area.RideControl]
+        }
+    }
+}
+
+class Visitor: Guest, Discount {
+    var foodDiscount: Percent
+    var merchDiscount: Percent
+    var type: GuestType
+    var age: Age?
+    
+    
+    
+    init(type: GuestType) {
+        self.type = type
+        
+        switch type {
+        case .Classic, .Child:
+            self.foodDiscount = 0
+            self.merchDiscount = 0
+        case .VIP:
+            self.foodDiscount = 10
+            self.merchDiscount = 20
+        }
+        
+    }
+    init(type: GuestType, age: Age) {
+        self.type = type
+        self.age = age
+        
+        switch type {
+        case .Classic, .Child:
+            self.foodDiscount = 0
+            self.merchDiscount = 0
+        case .VIP:
+            self.foodDiscount = 10
+            self.merchDiscount = 20
+        }
+    }
+    
+}
+
+class Manager: Administrator {
+    var firstName: String
+    var lastName: String
+    var adress: String
+    var city: String
+    var state: String
+    var zip: String
+    
+    init(first: String, last: String, adress: String, city: String, state: String, zip: String) {
+        self.firstName = first
+        self.lastName = last
+        self.adress = adress
+        self.city = city
+        self.state = state
+        self.zip = zip
+    }
+    
+    
+}
+
+
+
 
 
 
